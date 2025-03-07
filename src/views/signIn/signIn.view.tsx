1b972d839736de8da0478cd1ae3f58b8842eec40
hania <jasinska.hania@gmail.com>
2025-03-07 10:31:28 +0100

    loginAction mutation usage

--- src/views/signIn/signIn.view.tsx
+++ src/views/signIn/signIn.view.tsx
@@ -11,6 +11,7 @@ import { LoginModel, UserWithToken } from '../../models';
 import { useSignInValidation } from './signIn.hooks';
 import { RoutingPath } from '../../enums';
 import { loginRequest } from '../../api';
+import { useMutation, UseMutationResult } from '@tanstack/react-query';
 
 export const SignInView: React.FC = (): React.JSX.Element => {
   const { t } = useTranslation();
@@ -19,15 +20,17 @@ export const SignInView: React.FC = (): React.JSX.Element => {
   const { signIn }: UseAuth = useAuth();
   const navigate: NavigateFunction = useNavigate();
 
-  const loginAction: (data: LoginModel) => void = (data: LoginModel): void => {
-    loginRequest(data)
-      .then((response: UserWithToken): void => {
-        signIn(response.user, response.jwt);
-        navigate(RoutingPath.Home);
-      })
-      // TODO: add error handler
-      .catch((): void => undefined);
-  };
+  const mutation: UseMutationResult<UserWithToken, Error, LoginModel> = useMutation({
+    mutationFn: (data: LoginModel) => loginRequest(data),
+    onError: () => {
+      // add error handling!
+      (): void => undefined
+    },
+    onSuccess: (response: UserWithToken) => {
+      signIn(response.user, response.jwt);
+      navigate(RoutingPath.Home);
+    },
+  })
 
   return (
     <Container>
@@ -35,7 +38,7 @@ export const SignInView: React.FC = (): React.JSX.Element => {
         <Header>{t('app.views.signIn.header')}</Header>
         <Formik
           initialValues={{ identifier: '', password: '' }}
-          onSubmit={loginAction}
+          onSubmit={mutation.mutate}
           validateOnBlur={isFormSubmitted}
           validateOnChange={isFormSubmitted}
           validationSchema={SignInFormValidationSchema}
